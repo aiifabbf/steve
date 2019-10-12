cb8df44fdd6711d526edcb2bfe97d96c3d8563a8
Benjamin Shi <aiifabbf@outlook.com>
2019-10-12 11:23:38 -0500

    first working version with engine.js

--- engine.ts
+++ engine.ts
@@ -1,18 +1,181 @@
 import { mat4 } from "gl-matrix";
 
 export class Sprite {
-    vertices: Float32Array;
+    geometry: Geometry;
+    material: Material;
+    children: Array<Sprite>;
     modelMatrix: mat4;
-    shaderProgram: WebGLProgram;
 
-    constructor(vertices, modelMatrix, children, shaderProgram) {
-        this.vertices = vertices;
-        this.modelMatrix = modelMatrix;
-        this.shaderProgram = shaderProgram;
+    constructor(geometry: Geometry, material: Material) {
+        this.geometry = geometry;
+        this.material = material;
+
+        this.modelMatrix = mat4.create();
     }
 
-    draw(gl: WebGL2RenderingContext) {
-        gl.useProgram(this.shaderProgram);
+    draw(renderer: Renderer) {
+        let gl = renderer.gl;
+
+        gl.uniformMatrix4fv(
+            this.material.programInfo.uniformLocations["uModelViewMatrix"],
+            false,
+            this.modelMatrix,
+        );
+
+        gl.drawArrays(this.geometry.mode, 0, this.geometry.vertexPositions.length / 4);
+    }
+}
+
+export class Geometry {
+    vertexPositions: Array<Number>;
+    mode: number;
+
+    constructor(vertexPositions: Array<Number>) {
+        this.vertexPositions = vertexPositions;
+    }
+}
+
+export class TriangleGeometry extends Geometry {
+    constructor(width: number, height: number) {
+        super([
+            width, 0, 0, 1,
+            0, height, 0, 1,
+            0, 0, 0, 1,
+        ]);
+        this.mode = WebGL2RenderingContext.TRIANGLES;
+    }
+}
+
+export class TetrahedronGeometry extends Geometry {
+    constructor(width: number, height: number, depth: number) {
+        super([
+            0, 0, 0, 1,
+            0, height, 0, 1,
+            width, 0, 0, 1,
+            0, 0, depth, 1,
+            0, 0, 0, 1,
+            0, height, 0, 1,
+        ]);
+        this.mode = WebGL2RenderingContext.TRIANGLE_STRIP;
+    }
+}
+
+export class LineGeometry extends Geometry {
+    constructor(start: Array<number>, end: Array<number>) {
+        super([
+            start[0], start[1], start[2], 1,
+            end[0], end[1], end[2], 1
+        ]);
+        this.mode = WebGL2RenderingContext.LINES;
+    }
+}
+
+export class Material {
+    programInfo: ProgramInfo;
+    vertexShaderSource: string;
+    fragmentShaderSource: string;
+    buffers: {
+        attributes: {},
+        uniforms: {}
+    };
+
+    constructor(vertexShaderSource: string, fragmentShaderSource: string) {
+        this.vertexShaderSource = vertexShaderSource;
+        this.fragmentShaderSource = fragmentShaderSource;
+        this.buffers = {
+            attributes: {},
+            uniforms: {},
+        };
+    }
+
+    compile(renderer: Renderer, attributePlaceholders: Object, uniformPlaceholders: Object) {
+        let self = this;
+        let gl = renderer.gl;
+        let shaderProgram = getShaderProgram(gl, this.vertexShaderSource, this.fragmentShaderSource);
+        this.programInfo = getProgramInfo(gl, shaderProgram, attributePlaceholders, uniformPlaceholders);
+
+        Object.entries(this.programInfo.attributeLocations).forEach(function (item) {
+            let k = item[0];
+            let v = item[1];
+            self.buffers.attributes[k] = gl.createBuffer();
+        })
+
+        Object.entries(this.programInfo.uniformLocations).forEach(function (item) {
+            let k = item[0];
+            let v = item[1];
+            self.buffers.uniforms[k] = gl.createBuffer();
+        })
+    }
+
+    bindPlaceholders(renderer: Renderer, attributePlaceholderValueMapping: Object, uniformPlaceholderValueMapping: Object) {
+        let self = this;
+        let gl = renderer.gl;
+
+        gl.useProgram(this.programInfo.program);
+
+        Object.entries(attributePlaceholderValueMapping).forEach(function (item) {
+            let k = item[0];
+            let v = item[1];
+            gl.bindBuffer(gl.ARRAY_BUFFER, self.buffers.attributes[k]);
+            gl.bufferData(gl.ARRAY_BUFFER, v, gl.STATIC_DRAW);
+            gl.vertexAttribPointer(
+                self.programInfo.attributeLocations[k],
+                4,
+                gl.FLOAT,
+                false,
+                0,
+                0
+            );
+            gl.enableVertexAttribArray(self.programInfo.attributeLocations[k]);
+        });
+
+        // Object.entries(uniformPlaceholderValueMapping).forEach(function (item) {
+        //     let k = item[0];
+        //     let v = item[1];
+        //     gl.bindBuffer(gl.ARRAY_BUFFER, self.buffers.uniforms[k]);
+        //     gl.bufferData(gl.ARRAY_BUFFER, v, gl.STATIC_DRAW);
+        //     gl.vertexAttribPointer(
+        //         self.programInfo.uniformLocations[k],
+        //         4,
+        //         gl.FLOAT,
+        //         false,
+        //         0,
+        //         0
+        //     );
+        //     gl.enableVertexAttribArray(self.programInfo.uniformLocations[k]);
+        // });
+    }
+}
+
+export class ColorMaterial extends Material {
+    
+}
+
+export class Renderer {
+    canvas: HTMLCanvasElement;
+    gl: WebGL2RenderingContext;
+
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+
+        let gl = canvas.getContext("webgl2");
+
+        if (gl == null) {
+            throw new Error("Can't create webgl2 context");
+        }
+
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.enable(gl.DEPTH_TEST);
+        gl.enable(gl.CULL_FACE);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        this.gl = gl;
+    }
+
+    render(world: Sprite) {
+        world.draw(this);
     }
 }
 
@@ -26,8 +189,7 @@ export function getShaderProgram(gl: WebGL2RenderingContext, vertexShaderSource:
     gl.linkProgram(shaderProgram);
 
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("link error");
-        return;
+        throw new Error("Link error");
     }
 
     return shaderProgram;
@@ -38,8 +200,7 @@ function getShader(gl: WebGL2RenderingContext, type: number, source: string): We
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.log("Compilation error " + gl.getShaderInfoLog(shader));
-        return null;
+        throw new Error("Compilation error " + gl.getShaderInfoLog(shader));
     }
     return shader;
 }
@@ -50,15 +211,19 @@ interface ProgramInfo {
     uniformLocations: Object,
 };
 
-export function getProgramInfo(gl: WebGL2RenderingContext, shaderProgram: WebGLProgram, attributePlaceholders: Array<string>, uniformPlaceholders: Array<string>): ProgramInfo {
+export function getProgramInfo(gl: WebGL2RenderingContext, shaderProgram: WebGLProgram, attributePlaceholders: Object, uniformPlaceholders: Object): ProgramInfo {
     let attributeLocations = {};
-    attributePlaceholders.forEach(function (v) {
-        attributeLocations[v] = gl.getAttribLocation(shaderProgram, v);
+    Object.entries(attributePlaceholders).forEach(function (item) {
+        let k = item[0];
+        let v = item[1];
+        attributeLocations[k] = gl.getAttribLocation(shaderProgram, v);
     });
 
     let uniformLocations = {};
-    uniformPlaceholders.forEach(function (v) {
-        uniformLocations[v] = gl.getUniformLocation(shaderProgram, v);
+    Object.entries(uniformPlaceholders).forEach(function (item) {
+        let k = item[0];
+        let v = item[1];
+        uniformLocations[k] = gl.getUniformLocation(shaderProgram, v);
     });
 
     return {
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { mat4 } from "gl-matrix";
 import * as engine from "./engine.ts";
+import { Renderer, TriangleGeometry, Material, Sprite, TetrahedronGeometry } from "./engine";
 
 function main() {
     let canvas = document.querySelector("canvas");
@@ -28,6 +29,7 @@ function main() {
     `;
     let fragmentShaderSource = `
         precision mediump float;
+
         varying vec4 vVertexColor;
 
         void main() {
@@ -35,107 +37,173 @@ function main() {
         }
     `;
 
-    // let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    // gl.shaderSource(vertexShader, vertexShaderSource);
-    // gl.compileShader(vertexShader);
-    // if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    //     alert("compiler error");
-    //     gl.deleteShader(vertexShader);
-    //     return;
-    // }
+    // // let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    // // gl.shaderSource(vertexShader, vertexShaderSource);
+    // // gl.compileShader(vertexShader);
+    // // if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    // //     alert("compiler error");
+    // //     gl.deleteShader(vertexShader);
+    // //     return;
+    // // }
 
-    // let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    // gl.shaderSource(fragmentShader, fragmentShaderSource);
-    // gl.compileShader(fragmentShader);
-    // if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    //     alert("compile error");
-    //     gl.deleteShader(fragmentShader);
-    //     return;
-    // }
+    // // let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    // // gl.shaderSource(fragmentShader, fragmentShaderSource);
+    // // gl.compileShader(fragmentShader);
+    // // if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    // //     alert("compile error");
+    // //     gl.deleteShader(fragmentShader);
+    // //     return;
+    // // }
 
-    // let shaderProgram = gl.createProgram();
-    // gl.attachShader(shaderProgram, vertexShader);
-    // gl.attachShader(shaderProgram, fragmentShader);
-    // gl.linkProgram(shaderProgram);
+    // // let shaderProgram = gl.createProgram();
+    // // gl.attachShader(shaderProgram, vertexShader);
+    // // gl.attachShader(shaderProgram, fragmentShader);
+    // // gl.linkProgram(shaderProgram);
 
-    // if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    //     alert("link error");
-    //     return;
-    // }
+    // // if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    // //     alert("link error");
+    // //     return;
+    // // }
 
-    // replace with engine.getShaderProgram()
-    let shaderProgram = engine.getShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
-    console.log(shaderProgram);
+    // // replace with engine.getShaderProgram()
+    // let shaderProgram = engine.getShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
+    // console.log(shaderProgram);
 
-    // let programInfo = {
-    //     program: shaderProgram,
-    //     attributeLocations: {
-    //         vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-    //         vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
-    //     },
-    //     uniformLocations: {
-    //         modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-    //     }
-    // };
+    // // let programInfo = {
+    // //     program: shaderProgram,
+    // //     attributeLocations: {
+    // //         vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+    // //         vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
+    // //     },
+    // //     uniformLocations: {
+    // //         modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+    // //     }
+    // // };
+    // // console.log(programInfo);
+
+    // replace with renderer, material
+    let renderer = new Renderer(canvas);
+    let material = new Material(vertexShaderSource, fragmentShaderSource);
+    material.compile(renderer, {
+        aVertexPosition: "aVertexPosition",
+        aVertexColor: "aVertexColor",
+    }, {
+        uModelViewMatrix: "uModelViewMatrix",
+    });
+    let programInfo = material.programInfo;
+    // programInfo is correct
+
+    // // replace with engine.getProgramInfo()
+    // let programInfo = engine.getProgramInfo(gl, shaderProgram, {
+    //     aVertexPosition: "aVertexPosition",
+    //     aVertexColor: "aVertexColor"
+    // }, {
+    //     uModelViewMatrix: "uModelViewMatrix"
+    // });
     // console.log(programInfo);
 
-    // replace with engine.getProgramInfo()
-    let programInfo = engine.getProgramInfo(gl, shaderProgram, [
-        "aVertexPosition",
-        "aVertexColor",
-    ], [
-        "uModelViewMatrix"
-    ]);
-    console.log(programInfo);
+    // let vertexPositions = new Float32Array([
+    //     -0.5, -0.5, 0.0, 1.0, // vertex 0
+    //     0.5, -0.5, 0.0, 1.0, // vertex 1
+    //     0.5, 0.5, 0.0, 1.0, // vertex 2
+    // ]);
 
-    let vertexPositions = new Float32Array([
-        -0.5, -0.5, 0.0, 1.0, // vertex 0
-        0.5, -0.5, 0.0, 1.0, // vertex 1
-        0.5, 0.5, 0.0, 1.0, // vertex 2
-    ]);
+    // let vertexColors = new Float32Array([
+    //     1, 0, 0, 1,
+    //     0, 1, 0, 1,
+    //     0, 0, 1, 1
+    // ]);
 
-    let vertexColors = new Float32Array([
-        1, 0, 0, 1,
-        0, 1, 0, 1,
-        0, 0, 1, 1
-    ]);
+    // replace with geometry
+    let geometry = new TriangleGeometry(0.5, 0.5);
+    let triangle = new Sprite(geometry, material);
+    material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(geometry.vertexPositions),
+        aVertexColor: new Float32Array([
+            1, 0, 0, 1,
+            0, 1, 0, 1,
+            0, 0, 1, 1
+        ])
+    })
 
-    let vertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STATIC_DRAW);
-    gl.vertexAttribPointer(
-        programInfo.attributeLocations.aVertexPosition,
-        4,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
-    gl.enableVertexAttribArray(programInfo.attributeLocations.aVertexPosition);
+    // gl.useProgram(programInfo.program);
 
-    let vertexColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW);
-    gl.vertexAttribPointer(
-        programInfo.attributeLocations.aVertexColor,
-        4,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
-    gl.enableVertexAttribArray(programInfo.attributeLocations.aVertexColor);
+    // let vertexPositionBuffer = gl.createBuffer();
+    // gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STATIC_DRAW);
+    // gl.vertexAttribPointer(
+    //     programInfo.attributeLocations.aVertexPosition,
+    //     4,
+    //     gl.FLOAT,
+    //     false,
+    //     0,
+    //     0
+    // );
+    // gl.enableVertexAttribArray(programInfo.attributeLocations.aVertexPosition);
 
-    gl.useProgram(programInfo.program);
+    // let vertexColorBuffer = gl.createBuffer();
+    // gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW);
+    // gl.vertexAttribPointer(
+    //     programInfo.attributeLocations.aVertexColor,
+    //     4,
+    //     gl.FLOAT,
+    //     false,
+    //     0,
+    //     0
+    // );
+    // gl.enableVertexAttribArray(programInfo.attributeLocations.aVertexColor);
 
-    let modelViewMatrix = mat4.create();
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.uModelViewMatrix,
-        false,
-        modelViewMatrix
-    );
+    // let modelViewMatrix = mat4.create();
+    // gl.uniformMatrix4fv(
+    //     programInfo.uniformLocations.uModelViewMatrix,
+    //     false,
+    //     modelViewMatrix
+    // );
+    renderer.render(triangle);
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPositions.length / 4);
+    mat4.translate(triangle.modelMatrix, triangle.modelMatrix, [-0.5, -0.5, 0, 1]);
+    material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(triangle.geometry.vertexPositions),
+        aVertexColor: new Float32Array([
+            1, 0, 0, 1,
+            0, 1, 0, 1,
+            0, 0, 1, 1
+        ])
+    });
+    renderer.render(triangle);
+
+    let tetrahedron = new Sprite(new TetrahedronGeometry(0.5, 0.5, 0.5), material);
+    material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(tetrahedron.geometry.vertexPositions),
+        aVertexColor: new Float32Array([
+            1, 0, 0, 1,
+            0, 0, 1, 1,
+            0, 1, 0, 1,
+            1, 0, 1, 1,
+            1, 0, 0, 1,
+            0, 0, 1, 1,
+        ])
+    });
+    renderer.render(tetrahedron);
+    
+    function onDraw() {
+        mat4.rotateX(tetrahedron.modelMatrix, tetrahedron.modelMatrix, 0.1);
+        mat4.rotateZ(tetrahedron.modelMatrix, tetrahedron.modelMatrix, 0.1);
+        renderer.render(tetrahedron);
+
+        requestAnimationFrame(onDraw);
+    }
+
+    // onDraw();
+
+    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, geometry.vertexPositions.length / 4);
+
+    // let renderer = new Renderer(canvas);
+    // let geometry = new TriangleGeometry(0.5, 0.5);
+    // let material = new Material(vertexShaderSource, fragmentShaderSource);
+    // let triangle = new Sprite(geometry, material);
+    // renderer.render(triangle);
 }
 
 window.onload = main;
