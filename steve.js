3cee8084840aaf9124c650c67d4464d328a8235d
Virgil_Chen <virgilchen97@gmai.com>
2019-10-14 20:34:19 -0500

    Add moving arms

--- steve.js
+++ steve.js
@@ -48,54 +48,80 @@ function main() {
         }, {});
     });
 
-    let body = new Sprite(new CubeGeometry(0.16, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    let body = new Sprite(new CubeGeometry(0.16, 0.24, 0.08), new ColorMaterial([0,0.686,0.686,1]));
     body.material.compile(renderer);
     body.material.bindPlaceholders(renderer, {
         aVertexPosition: new Float32Array(body.geometry.vertexPositions),
     }, {});
     
-    let head = new Sprite(new CubeGeometry(0.16, 0.16, 0.16), new ColorMaterial([1,1,1,1]));
+    let headJiont = new Sprite(null,null);
+    let head = new Sprite(new CubeGeometry(0.16, 0.16, 0.16), new ColorMaterial([0.702,0.482,0.384,1]));
     head.material.compile(renderer);
     head.material.bindPlaceholders(renderer, {
         aVertexPosition: new Float32Array(head.geometry.vertexPositions),
     }, {});
 
-    let larm = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    let larmJiont = new Sprite(null,null);
+    let larm = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([0.588,0.372,0.255,1]));
     larm.material.compile(renderer);
     larm.material.bindPlaceholders(renderer, {
         aVertexPosition: new Float32Array(larm.geometry.vertexPositions),
     }, {});
 
-    let rarm = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    let rarmJiont = new Sprite(null,null);
+    let rarm = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([0.588,0.372,0.255,1]));
     rarm.material.compile(renderer);
     rarm.material.bindPlaceholders(renderer, {
         aVertexPosition: new Float32Array(rarm.geometry.vertexPositions),
     }, {});
 
-    let lleg = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    let llegJiont = new Sprite(null,null);
+    let lleg = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([0.275,0.228,0.647,1]));
     lleg.material.compile(renderer);
     lleg.material.bindPlaceholders(renderer, {
         aVertexPosition: new Float32Array(lleg.geometry.vertexPositions),
     }, {});
 
-    let rleg = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    let rlegJiont = new Sprite(null,null);
+    let rleg = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([0.275,0.228,0.5,1]));
     rleg.material.compile(renderer);
     rleg.material.bindPlaceholders(renderer, {
         aVertexPosition: new Float32Array(rleg.geometry.vertexPositions),
     }, {});
 
-    //world.add(body);
-    mat4.translate(larm.modelViewMatrix, larm.modelViewMatrix, [0.12,0,0]);
-    mat4.translate(rarm.modelViewMatrix, rarm.modelViewMatrix, [-0.12,0,0]);
-    mat4.translate(lleg.modelViewMatrix, lleg.modelViewMatrix, [-0.04,-0.24,0]);
-    mat4.translate(rleg.modelViewMatrix, rleg.modelViewMatrix, [0.04,-0.24,0]);
-    mat4.translate(head.modelViewMatrix, head.modelViewMatrix, [0,0.2,0]);
     world.add(body);
-    body.add(larm);
-    body.add(rarm);
-    body.add(head);
-    body.add(lleg);
-    body.add(rleg);
+
+    // larm
+    mat4.translate(larmJiont.modelViewMatrix, larmJiont.modelViewMatrix, [0.12,0.08,0]);
+    mat4.translate(larm.modelViewMatrix, larm.modelViewMatrix, [0,-0.08,0]);
+    body.add(larmJiont);
+    larmJiont.add(larm);
+
+    // rarm
+    mat4.translate(rarmJiont.modelViewMatrix, rarmJiont.modelViewMatrix, [-0.12,0.08,0]);
+    mat4.translate(rarm.modelViewMatrix, rarm.modelViewMatrix, [0,-0.08,0]);
+    body.add(rarmJiont);
+    rarmJiont.add(rarm);
+
+    // lleg
+    mat4.translate(llegJiont.modelViewMatrix, llegJiont.modelViewMatrix, [-0.04,-0.12,0]);
+    mat4.translate(lleg.modelViewMatrix, lleg.modelViewMatrix, [0,-0.12,0]);
+    body.add(llegJiont);
+    llegJiont.add(lleg);
+
+    // rleg
+    mat4.translate(rlegJiont.modelViewMatrix, rlegJiont.modelViewMatrix, [0.04,-0.12,0]);
+    mat4.translate(rleg.modelViewMatrix, rleg.modelViewMatrix, [0,-0.12,0]);
+    body.add(rlegJiont);
+    rlegJiont.add(rleg);
+
+    // head
+    mat4.translate(headJiont.modelViewMatrix, headJiont.modelViewMatrix, [0,0.12,0]);
+    mat4.translate(head.modelViewMatrix, head.modelViewMatrix, [0,0.08,0]);
+    body.add(headJiont);
+    headJiont.add(head);
+    
+    
     world.add(xAxis);
     world.add(yAxis);
     world.add(zAxis);
@@ -112,6 +138,15 @@ function main() {
         let percent = document.querySelector("input[id=bodyPosition]").value / 100;
         mat4.identity(body.modelViewMatrix);
         mat4.translate(body.modelViewMatrix, body.modelViewMatrix, [0, 0, percent]);
+
+        // arm rotation
+        let armRotation = document.querySelector("input[id=armPosition]").value / 100;
+        mat4.identity(larmJiont.modelViewMatrix);
+        mat4.translate(larmJiont.modelViewMatrix, larmJiont.modelViewMatrix, [0.12,0.08,0]);
+        mat4.rotateX(larmJiont.modelViewMatrix, larmJiont.modelViewMatrix, armRotation);
+        mat4.identity(rarmJiont.modelViewMatrix);
+        mat4.translate(rarmJiont.modelViewMatrix, rarmJiont.modelViewMatrix, [-0.12,0.08,0]);
+        mat4.rotateX(rarmJiont.modelViewMatrix, rarmJiont.modelViewMatrix, -armRotation);
         
         renderer.clear();
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
