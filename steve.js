48a5dc5e4468f0cd2c0e00a829810ad056a9d6c5
Virgil_Chen <virgilchen97@gmai.com>
2019-10-14 19:13:59 -0500

    Add real steve geometry to the world

--- steve.js
+++ steve.js
@@ -48,29 +48,71 @@ function main() {
         }, {});
     });
 
-    let cube = new Sprite(new CubeGeometry(0.16, 0.24, 0.1), new ColorMaterial([1,1,1,1]));
-    cube.material.compile(renderer);
-    cube.material.bindPlaceholders(renderer, {
-        aVertexPosition: new Float32Array(cube.geometry.vertexPositions),
+    let body = new Sprite(new CubeGeometry(0.16, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    body.material.compile(renderer);
+    body.material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(body.geometry.vertexPositions),
+    }, {});
+    
+    let head = new Sprite(new CubeGeometry(0.16, 0.16, 0.16), new ColorMaterial([1,1,1,1]));
+    head.material.compile(renderer);
+    head.material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(head.geometry.vertexPositions),
     }, {});
 
-    world.add(cube);
+    let larm = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    larm.material.compile(renderer);
+    larm.material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(larm.geometry.vertexPositions),
+    }, {});
+
+    let rarm = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    rarm.material.compile(renderer);
+    rarm.material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(rarm.geometry.vertexPositions),
+    }, {});
+
+    let lleg = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    lleg.material.compile(renderer);
+    lleg.material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(lleg.geometry.vertexPositions),
+    }, {});
+
+    let rleg = new Sprite(new CubeGeometry(0.08, 0.24, 0.08), new ColorMaterial([1,1,1,1]));
+    rleg.material.compile(renderer);
+    rleg.material.bindPlaceholders(renderer, {
+        aVertexPosition: new Float32Array(rleg.geometry.vertexPositions),
+    }, {});
+
+    //world.add(body);
+    mat4.translate(larm.modelViewMatrix, larm.modelViewMatrix, [0.12,0,0]);
+    mat4.translate(rarm.modelViewMatrix, rarm.modelViewMatrix, [-0.12,0,0]);
+    mat4.translate(lleg.modelViewMatrix, lleg.modelViewMatrix, [-0.04,-0.24,0]);
+    mat4.translate(rleg.modelViewMatrix, rleg.modelViewMatrix, [0.04,-0.24,0]);
+    mat4.translate(head.modelViewMatrix, head.modelViewMatrix, [0,0.2,0]);
+    world.add(body);
+    body.add(larm);
+    body.add(rarm);
+    body.add(head);
+    body.add(lleg);
+    body.add(rleg);
     world.add(xAxis);
     world.add(yAxis);
     world.add(zAxis);
-
     
-    mat4.rotateX(world.modelViewMatrix, world.modelViewMatrix, 45);
+    mat4.rotateX(world.modelViewMatrix, world.modelViewMatrix, 6);
     mat4.rotateY(world.modelViewMatrix, world.modelViewMatrix, -45);
     renderer.render(world);
 
     function onDraw() {
-        //mat4.rotateZ(triangle2.modelViewMatrix, triangle2.modelViewMatrix, 0.02);
-        //mat4.rotateZ(tetrahedron.modelViewMatrix, tetrahedron.modelViewMatrix, 0.03);
+        // world rotation
         mat4.rotateY(world.modelViewMatrix, world.modelViewMatrix, -0.01);
-        let percent = document.querySelector("input[type=range]").value / 100;
-        mat4.identity(cube.modelViewMatrix);
-        mat4.translate(cube.modelViewMatrix, cube.modelViewMatrix, [0, 0, percent]);
+        
+        // body position
+        let percent = document.querySelector("input[id=bodyPosition]").value / 100;
+        mat4.identity(body.modelViewMatrix);
+        mat4.translate(body.modelViewMatrix, body.modelViewMatrix, [0, 0, percent]);
+        
         renderer.clear();
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         renderer.render(world);
